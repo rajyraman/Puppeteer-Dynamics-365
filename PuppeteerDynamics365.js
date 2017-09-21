91752f748337c1bec3663597d210b9e7dd3a9e39
Natraj Yegnaraman <rajyraman@gmail.com>
2017-09-21 10:23:04 +1000

    1. Grab all command when navigating to a page with command bar 2. use waitForNav while waiting for page to load

--- PuppeteerDynamics365.js
+++ PuppeteerDynamics365.js
@@ -1,126 +1,143 @@
 const puppeteer = require('Puppeteer'),
-      cp = require('child_process');
+cp = require('child_process');
 
 
 require('dotenv').config();
 
 class PuppeteerDynamics365 {
 
-    constructor(url){
-        this._url = url;
-    }
+constructor(url) {
+    this._url = url;
+}
 
-    async start(){
-        this._browser = await puppeteer.launch({
-            headless: false,
-            ignoreHTTPSErrors: true
-            });
-        this._page = await this._browser.newPage();       
-        this._page.setViewport({width: 1920, height: 1080});
-        this._takeScreenshot = async (fileName, waitForNavigation)=>{
-            if(waitForNavigation){
-                await this._page.waitForNavigation({waitUntil: 'networkidle', networkIdleTimeout: 2000})
-            }
-            await this._page.screenshot({path: fileName});
-        };
-        this._annotateScreenshot = async (label, fileName)=>{
-            cp.execSync(`magick -size 600x100 -background blue -font Calibri -pointsize 40 -fill white -gravity center label:"${label}" -bordercolor red -border 8x4 -trim "${fileName}" +swap -gravity south -composite "Annotated-${fileName}"`,
-            {cwd: __dirname}); 
-        };      
-        
-        return await this.navigateTo();
-    }
-    async navigateTo(url){
-        await this._page.goto(url || this._url, {waitUntil: 'networkidle'});
-        let mainFrame = this._page.mainFrame();
-        let childFrames = mainFrame.childFrames();
-        //dismiss any popups
-        let dialogFrame = childFrames.find(x=>x.name() === 'InlineDialog_Iframe');
-        await this._page.evaluate(() => {
-          let popup = document.querySelector('#InlineDialog_Iframe');
-          if(popup){
-            popup.contentDocument.querySelector('#butBegin').click();
-          }
-        });
-        await this._page.click(process.env.TAB_HOME);
-        let groups = await this._page.evaluate(() => Array.from(document.querySelectorAll('.nav-group > .navActionButtonContainer')).map(x=>({id: x.id, label: x.querySelector('.navActionButtonLabel').innerText})));
-        await this._page.click(process.env.TAB_HOME);
-        return groups;
-    }
-    async navigateToGroup(groups, groupName, screenshotPrefs) {
-        let selectedGroup = groups.find(s=>s.label === groupName);
-        if(!selectedGroup) {
-            Promise.reject(new Error('Not a valid group'));
+async start() {
+    this._browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true
+    });
+    this._page = await this._browser.newPage();
+    this._page.setViewport({ width: 1920, height: 1080 });
+
+    this._takeScreenshot = async (fileName, waitForNavigation) => {
+        if (waitForNavigation) {
+            await this._page.waitForNavigation({ waitUntil: 'networkidle', networkIdleTimeout: 2000 });
         }
-        let selectedGroupId = `#${selectedGroup.id}`, 
-            selectedGroupLabel = selectedGroup.label;
-        
-        await this._page.click(process.env.TAB_HOME);
-        await this._page.click(selectedGroupId, {delay: 500});
-        await this._page.click(selectedGroupId, {delay: 500});
-        if(screenshotPrefs){
-            let fileName = `${screenshotPrefs.fileName || selectedGroup.label}.png`;
+        await this._page.screenshot({ path: fileName });
+    };
+
+    this._annotateScreenshot = async (label, fileName) => {
+        cp.execSync(`magick -size 600x100 -background blue -font Calibri -pointsize 40 -fill white -gravity center label:"${label}" -bordercolor red -border 8x4 -trim "${fileName}" +swap -gravity south -composite "Annotated-${fileName}"`,
+            { cwd: __dirname });
+    };
+
+    this._clickItem = async (rules, screenshotPrefs) => {
+        rules = Object.assign({ homeClick: false, waitForNav: true, clickCount: 1 }, rules);
+        let selectedItem = rules.items.find(s => s.label === rules.itemName);
+        if (!selectedItem) {
+            Promise.reject(new Error('Not a valid item'));
+        }
+        let selectedItemId = rules.selector ? rules.selector.replace('{0}', `${selectedItem.id}`) : `#${selectedItem.id}`,
+            selectedItemLabel = selectedItem.label;
+        if (rules.homeClick) {
+            await this._page.click(process.env.TAB_HOME);
+        }
+        for (let i = 1; i <= rules.clickCount; i++) {
+            await this._page.click(selectedItemId, { delay: 500 });
+        }
+        if (rules.waitForNav) {
+            // await this._page.waitFor(4000);
+            try{
+                await this._page.waitForNavigation({timeout: 6000});
+            }catch(e){}
+            //await this._page.waitForNavigation({waitUntil: 'networkidle', networkIdleTimeout: 2000});                                
+        }
+        if (screenshotPrefs) {
+            let fileName = `${screenshotPrefs.fileName || selectedItem.label}.png`;
             await this._takeScreenshot(fileName);
 
-            if(screenshotPrefs.annotateText){
+            if (screenshotPrefs.annotateText) {
                 await this._annotateScreenshot(screenshotPrefs.annotateText, fileName);
             }
         }
-        return await this._page.evaluate(() => Array.from(document.querySelectorAll('.nav-subgroup > .nav-rowBody')).map(x=>({id: x.id, label: x.querySelector('.nav-rowLabel').innerText})));
+    };
+
+    return await this.navigateTo();
+}
+async navigateTo(url) {
+    await this._page.goto(url || this._url, { waitUntil: 'networkidle' });
+    let mainFrame = this._page.mainFrame();
+    let childFrames = mainFrame.childFrames();
+    //dismiss any popups
+    let dialogFrame = childFrames.find(x => x.name() === 'InlineDialog_Iframe');
+    await this._page.evaluate(() => {
+        let popup = document.querySelector('#InlineDialog_Iframe');
+        if (popup) {
+            popup.contentDocument.querySelector('#butBegin').click();
+        }
+    });
+    await this._page.click(process.env.TAB_HOME);
+    let groups = await this._page.evaluate(() => Array.from(document.querySelectorAll('.nav-group > .navActionButtonContainer')).map(x => ({ id: x.id, label: x.querySelector('.navActionButtonLabel').innerText })));
+    await this._page.click(process.env.TAB_HOME);
+    return groups;
+}
+async navigateToGroup(groups, groupName, screenshotPrefs) {
+    await this._clickItem({
+        items: groups,
+        itemName: groupName,
+        homeClick: true,
+        waitForNav: false,
+        clickCount: 2
+    }, screenshotPrefs);
+    return await this._page.evaluate(() => Array.from(document.querySelectorAll('.nav-subgroup > .nav-rowBody')).map(x => ({ id: x.id, label: x.querySelector('.nav-rowLabel').innerText })));
+}
+
+async navigateToSubgroup(subgroups, subgroupName, screenshotPrefs) {
+    let commands = [];
+    await this._clickItem({
+        items: subgroups,
+        itemName: subgroupName,
+        clickCount: 2
+    }, screenshotPrefs);
+    try{
+        await this._page.waitForSelector('#crmRibbonManager',{visible: true, timeout: 6000});
+        commands = await this._page.evaluate(() => Array.from(document.querySelectorAll('.ms-crm-CommandBarItem')).map(x => ({ id: x.id, label: x.querySelector('a span') ? x.querySelector('a span').innerText : '' })));
+        if(commands.find(c => c.id === 'moreCommands')){
+            await this._clickItem({
+                items: commands,
+                waitForNav: false,
+                itemName: '',
+                selector: '.ms-crm-CommandBarItem[id="moreCommands"] a.ms-crm-Menu-Label'
+            });
+            commands = await this._page.evaluate(() => Array.from(document.querySelectorAll('.ms-crm-CommandBarItem')).map(x => ({ id: x.id, label: x.querySelector('a span') ? x.querySelector('a span').innerText : '' })));
+        }            
+    }catch(e){
+        console.log(`No visible ribbon for ${subgroupName}`);
     }
+    return commands;
+}
 
-    async navigateToSubgroup(subgroups, subgroupName, screenshotPrefs){
-        let selectedSubgroup = subgroups.find(s=>s.label === subgroupName);
-        if(!selectedSubgroup) {
-            Promise.reject(new Error('Not a valid subgroup'));
-        }
-        let selectedSubgroupId = `#${selectedSubgroup.id}`, 
-            selectedSubgroupLabel = selectedSubgroup.label;
-        await this._page.click(selectedSubgroupId);
-        if(screenshotPrefs){
-            let fileName = `${screenshotPrefs.fileName || selectedSubgroup.label}.png`;
-            await this._takeScreenshot(fileName, true);
+async clickCommandBarButton(commands, commandName, screenshotPrefs) {
+    await this._clickItem({
+        items: commands,
+        itemName: commandName,
+        clickCount: 2,
+        selector: '.ms-crm-CommandBarItem[id="{0}"] a.ms-crm-Menu-Label'
+    }, screenshotPrefs);
+}
 
-            if(screenshotPrefs.annotateText){
-                await this._annotateScreenshot(screenshotPrefs.annotateText, fileName);
-            }
-        }
-        await this._page.waitForNavigation({waitUntil: 'networkidle', networkIdleTimeout: 1000});
-        return await this._page.evaluate(()=>Array.from(document.querySelectorAll('.ms-crm-CommandBarItem')).map(x=>({id: x.id, label: x.querySelector('a span') ? x.querySelector('a span').innerText : ''})));    
-    }
-
-    async clickCommandBarButton(commands, commandName, screenshotPrefs){
-        let selectedCommand = commands.find(s=>s.label === commandName);
-        if(!selectedCommand) {
-            Promise.reject(new Error('Not a valid command bar button'));
-        }
-        let selectedCommandId = `.ms-crm-CommandBarItem[id="${selectedCommand.id}"] a.ms-crm-Menu-Label`, 
-            selectedCommandLabel = selectedCommand.label;
-        await this._page.click(selectedCommandId);
-        await this._page.waitForNavigation({waitUntil: 'networkidle', networkIdleTimeout: 1000});
-        if(screenshotPrefs){
-            let fileName = `${screenshotPrefs.fileName || selectedCommand.label}.png`;
-            await this._takeScreenshot(fileName, true);
-
-            if(screenshotPrefs.annotateText){
-                await this._annotateScreenshot(screenshotPrefs.annotateText, fileName);
-            }
-        }
-    }
-
-    async getAttributes(){
-        let attributes = await this._page.evaluate(()=> {
-            let xrm = Array.from(Array.from(document.querySelectorAll('iframe')).find(x=>x.style.visibility === 'visible')
-            .contentDocument.querySelectorAll('iframe')).find(x=>x.id === 'customScriptsFrame').contentWindow
+async getAttributes() {
+    let attributes = await this._page.evaluate(() => {
+        let xrm = Array.from(Array.from(document.querySelectorAll('iframe')).find(x => x.style.visibility === 'visible')
+            .contentDocument.querySelectorAll('iframe')).find(x => x.id === 'customScriptsFrame').contentWindow
             .Xrm
-            return xrm.Page.getAttribute().map(x=>x.getName());
-        });
-        return attributes;
-    }
+        return xrm.Page.getAttribute().map(x => x.getName());
+    });
+    return attributes;
+}
 
-    exit(){
-        this._browser.close();
-    }
+exit() {
+    this._browser.close();
+}
 }
 
 module.exports = PuppeteerDynamics365;
--- index.js
+++ index.js
@@ -4,15 +4,22 @@ require('dotenv').config();
 
 (async () => {
   const pup = new PuppeteerDynamics365(process.env.CRM_URL);
-  let groups = await pup.start();
-
-  let subgroups = await pup.navigateToGroup(groups, 'Workspace', {annotateText:'Workspace'});
-  let commands = await pup.navigateToSubgroup(subgroups, 'Clients', {annotateText:'Clients view'})
-  await pup.clickCommandBarButton(commands, 'NEW', {annotateText:'Create new client', fileName: 'Create new client'});
-  let attributes = await pup.getAttributes();
-  console.log(attributes);
-  subgroups = await pup.navigateToGroup(groups, 'Settings');
-  commands = await pup.navigateToSubgroup(subgroups, 'Administration', {annotateText:'Administration area'});  
-  console.log(commands);
+  try{
+    let groups = await pup.start();
+    let subgroups = await pup.navigateToGroup(groups, 'Workspace', {annotateText:'Workspace'});
+    let commands = await pup.navigateToSubgroup(subgroups, 'Clients', {annotateText:'Clients view'});
+    console.log(commands);
+    await pup.clickCommandBarButton(commands, 'NEW', {annotateText:'Create new client', fileName: 'Create new client'});
+    let attributes = await pup.getAttributes();
+    console.log(attributes);
+    subgroups = await pup.navigateToGroup(groups, 'Settings');
+    commands = await pup.navigateToSubgroup(subgroups, 'Administration', {annotateText:'Administration area'}); 
+    
+    subgroups = await pup.navigateToGroup(groups, 'Marketing');
+    await pup.navigateToSubgroup(subgroups, 'Leads', {annotateText:'View all existing active leads'});
+      
+  }catch(e){
+    console.log(e);
+  }  
   pup.exit();
 })();
